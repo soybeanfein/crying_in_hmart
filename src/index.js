4cefc404ee9988d0b0fbe32c2c6f18fdf7b3e3d6
Rich Yang <richyang@Sophias-MacBook-Air-2.local>
2025-03-17 11:17:11 -0400

    y photos

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import { App } from './App'
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+import { App } from './App';
 
-const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
+// Replace these URLs with your own image URLs
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
-]
+  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' },
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: 'https://asianamericanmusics17.wordpress.com/wp-content/uploads/2017/04/approved20by20michelle.jpeg?w=523&h=349' }
+];
 
-createRoot(document.getElementById('root')).render(<App images={images} />)
+createRoot(document.getElementById('root')).render(<App images={images} />);
