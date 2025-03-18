d48de9983802a060be35e406ce712354adf232ca
Rich Yang <richyang@Sophias-MacBook-Air-2.local>
2025-03-18 08:36:25 -0400

    hello

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { App } from './App';
 // Replace these URLs with your own image URLs
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: 'https://static01.nyt.com/images/2022/06/12/magazine/12mag-talk-03/12mag-talk-03-superJumbo.jpg' },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: '/photo4.jpeg', scale: [1.5, 1, 1]
+  },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: 'https://static01.nyt.com/images/2022/06/12/magazine/12mag-talk-03/12mag-talk-03-superJumbo.jpg' },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: 'https://static01.nyt.com/images/2022/06/12/magazine/12mag-talk-03/12mag-talk-03-superJumbo.jpg' },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: '/photo1.png', scale: [1, 1, 1] },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: '/photo1.png', scale: [1, 1, 1] },
   // Left
   { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: 'https://static01.nyt.com/images/2022/06/12/magazine/12mag-talk-03/12mag-talk-03-superJumbo.jpg' },
   { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: 'https://static01.nyt.com/images/2022/06/12/magazine/12mag-talk-03/12mag-talk-03-superJumbo.jpg' },
